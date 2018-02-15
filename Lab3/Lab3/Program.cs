b8bdaded892d101dcb33869e683e9a65c38c94b0
Taha <taha24888@gmail.com>
2018-02-16 00:01:29 +0500

    Params , ref & out attribute

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -10,6 +10,35 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
+            int i = 1;
+            int a;
+            method1(ref i);
+            Console.WriteLine("Value After Ref method: " + i);
+            method2(out a, i);
+            Console.WriteLine("Value After out method: " + a);
+            Console.WriteLine("Sum of all values Passed in ParamsMethod: " + ParamsMethod(1, 2, 3, 4, 4, 5, 6));
+
+            Console.ReadLine();
+
         }
+        public static void method1(ref int x)
+        {
+            x = x + 10;
+
+        }
+        public static void method2(out int a, int i)
+        {
+            a = i + 10;
+        }
+        public static int ParamsMethod(params int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+
     }
 }
